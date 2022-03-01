f5acb30bec8c17643174661da21d5d1e52cf5e57
Georgy Kantis <80484909+Georgy128@users.noreply.github.com>
2022-03-01 11:32:06 +0300

    Add files via upload

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/golang.org/x/example/stringutil"
+)
+
 func main() {
 	// Place your code here.
+
+	var who string // Переменная ввода
+	// Ввод строки
+	fmt.Println("Введите строку:")
+	fmt.Scanf("%s\n", &who)
+	// Проверяем на ""
+	if len(who) > 1 {
+		fmt.Println("Hello", who)
+		fmt.Println(stringutil.Reverse(who))
+		//who = strings.Join(os.Args[1:], " ")
+	}
+
 }
