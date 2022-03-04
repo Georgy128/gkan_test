ac5b0a436bf99b0bc915fa03df86881f5698dddd
Georgy Kantis <80484909+Georgy128@users.noreply.github.com>
2022-03-04 10:56:26 +0300

    Add files via upload

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/golang.org/x/example/stringutil"
+	//"github.com/golang.org/x/example/stringutil"
 )
 
 func main() {
 	// Place your code here.
 
-	var who string // Переменная ввода
+	var s string // Переменная ввода
 	// Ввод строки
 	fmt.Println("Введите строку:")
-	fmt.Scanf("%s\n", &who)
+	fmt.Scanf("%s\n", &s)
 	// Проверяем на ""
-	if len(who) > 1 {
-		fmt.Println("Hello", who)
-		fmt.Println(stringutil.Reverse(who))
-		//who = strings.Join(os.Args[1:], " ")
+	if len(s) > 1 {
+		fmt.Println(Reverse(s)) //stringutil.Reverse(s))
 	}
 
+}
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
